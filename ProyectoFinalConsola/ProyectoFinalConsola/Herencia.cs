4905c0c4f1f18f2c14ae88d0f4df585efcfe541f
I20050041 <71293482+I20050041@users.noreply.github.com>
2020-12-14 19:10:50 -0600

    Add files via upload

---
+++ ProyectoFinalConsola/ProyectoFinalConsola/Herencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalConsola 
+{
+    class Herencia
+    {
+
+        public struct productos
+        {
+            public float codigo;
+            public string nombre_producto;
+            public double precio_producto;
+            public string detalle_producto;
+            public DateTime fecha_ingreso;
+            public int cantidad;
+        }
+      
+    }
+}
+
